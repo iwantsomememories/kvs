d33b2815e100035cc00117f960a7907f7ec1cd84
fanqiechaodan <1484119655@qq.com>
2025-04-07 11:05:06 +0800

    Finish part 3 of Project4

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,11 +1,15 @@
 use clap::{Parser, ValueEnum};
+use kvs::thread_pool::{NaiveThreadPool, ThreadPool};
 use serde::{Deserialize, Serialize};
+use num_cpus;
 use std::fmt::Display;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::str::FromStr;
+use std::env;
 use std::env::current_dir;
 use std::process::exit;
 use std::fs::{File, OpenOptions};
+use std::sync::Arc;
 
 #[macro_use]
 extern crate slog;
@@ -62,37 +66,38 @@ fn main() {
     let drain = slog_term::CompactFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
 
-    let server = slog::Logger::root(drain, o!("kvs-server version" => env!("CARGO_PKG_VERSION")));
+    let server_logger = slog::Logger::root(drain, o!("kvs-server version" => env!("CARGO_PKG_VERSION")));
+    let server_logger = Arc::new(server_logger); 
 
     let cli = Cli::parse();
-    info!(server, "Listening on {}", cli.addr; "IP address" => cli.addr.ip().to_string(), "port" => cli.addr.port().to_string());
+    info!(server_logger, "Listening on {}", cli.addr; "IP address" => cli.addr.ip().to_string(), "port" => cli.addr.port().to_string());
 
     let cur_engine = match current_engine() {
         Ok(eng) => eng,
         Err(e) => {
-            warn!(server, "The content of engine file is invalid: {e}");
+            warn!(server_logger, "The content of engine file is invalid: {e}");
             None
         }
     };
 
     if cli.engine.is_some() && cur_engine.is_some() && cli.engine != cur_engine {
-        error!(server, "Wrong engine!");
-        drop(server);
+        error!(server_logger, "Wrong engine!");
+        drop(server_logger);
         exit(1);
     }
 
     let engine = cli.engine.unwrap_or(DEFAULT_STORAGE_ENGINE);
-    info!(server, "Storage Engine: {}", engine; "storage engine" => format!("{}", engine));
+    info!(server_logger, "Storage Engine: {}", engine; "storage engine" => format!("{}", engine));
 
-    let res = run(engine, cli.addr, &server);
+    let res = run(engine, cli.addr, server_logger.clone());
     if let Err(e) = res {
-        error!(server, "{}", e);
-        drop(server);
+        error!(server_logger, "{}", e);
+        drop(server_logger);
         exit(1);
     }
 }
 
-fn run(engine: Engine, addr: SocketAddr, logger: &Logger) -> Result<()> {
+fn run(engine: Engine, addr: SocketAddr, logger: Arc<Logger>) -> Result<()> {
     let engine_file = OpenOptions::new()
         .create(true)
         .write(true)
@@ -100,14 +105,17 @@ fn run(engine: Engine, addr: SocketAddr, logger: &Logger) -> Result<()> {
 
     serde_json::to_writer(engine_file, &engine)?;
 
+    let pool = NaiveThreadPool::new(num_cpus::get() as u32)?;
+
     match engine {
-        Engine::Kvs => run_with_engine(KvStore::open(current_dir()?)?, addr, logger),
-        Engine::Sled => run_with_engine(SledEngine::new(sled::open(current_dir()?)?), addr, logger),
+        Engine::Kvs => run_with_engine(KvStore::open(current_dir()?)?, pool, addr, logger),
+        Engine::Sled => run_with_engine(SledEngine::new(sled::open(current_dir()?)?), pool, addr, logger),
     }
 }
 
-fn run_with_engine<E: KvsEngine>(engine: E, addr: SocketAddr, logger: &Logger) -> Result<()> {
-    let server = KvsServer::new(engine);
+fn run_with_engine<E: KvsEngine, P: ThreadPool>(engine: E, pool: P, addr: SocketAddr, logger: Arc<Logger>) -> Result<()> {
+
+    let server = KvsServer::new(engine, pool);
     server.run(addr, logger)
 }
 
--- src/server.rs
+++ src/server.rs
@@ -1,83 +1,86 @@
 use crate::engines::KvsEngine;
 use crate::error::Result;
 use crate::common::*;
+use crate::thread_pool::ThreadPool;
 
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::io::{BufReader, BufWriter, Write};
+use std::sync::Arc;
 use serde_json::de::Deserializer;
 use slog::Logger;
 
 /// Kvs服务器
-pub struct KvsServer<E: KvsEngine> {
+pub struct KvsServer<E: KvsEngine, P: ThreadPool> {
     engine: E,
+    pool: P,
 }
 
-impl<E: KvsEngine> KvsServer<E> {
+impl<E: KvsEngine, P: ThreadPool> KvsServer<E, P> {
     /// 根据给定存储引擎生成一个Kvs服务器
-    pub fn new(engine: E) -> Self {
-        KvsServer { engine }
+    pub fn new(engine: E, pool: P) -> Self {
+        KvsServer { engine, pool }
     }
 
     /// 运行监听给定addr的Kvs服务器
-    pub fn run(mut self, addr: SocketAddr, logger: &Logger) -> Result<()> {
+    pub fn run(self, addr: SocketAddr, logger: Arc<Logger>) -> Result<()> {
         let listener = TcpListener::bind(addr)?;
 
         for stream in listener.incoming() {
-            match stream {
+            let engine = self.engine.clone();
+            let connection_logger = logger.clone();
+
+            self.pool.spawn(move || match stream {
                 Ok(stream) => {
-                    let client_addr = stream.peer_addr()?;
-                    info!(logger, "Received a connection from {}", client_addr);
-                    let connection_log = logger.new(o!("client addr" => client_addr));
-                    if let Err(e) = self.serve(stream, logger) {
-                        error!(connection_log, "Error on serving client: {}", e);
+                    if let Err(e) = serve(engine.clone(), stream, connection_logger.clone()) {
+                        error!(connection_logger, "Error on serving client: {}", e);
                     }
                 }
-                Err(e) => error!(logger, "Connection failed: {}", e),
-            }
+                Err(e) => error!(connection_logger, "Connection failed: {}", e),
+            });
         }
 
         Ok(())
     }
+}
 
-    fn serve(&mut self, tcp: TcpStream, logger: &Logger) -> Result<()> {
-        let peer_addr = tcp.peer_addr()?;
-        let tcp_cloned = tcp.try_clone()?;
-        let req_reader = Deserializer::from_reader(BufReader::new(&tcp)).into_iter::<Request>();
-        let mut writer = BufWriter::new(&tcp_cloned);
+fn serve<E: KvsEngine>(engine: E, tcp: TcpStream, logger: Arc<Logger>) -> Result<()> {
+    let peer_addr = tcp.peer_addr()?;
+    let tcp_cloned = tcp.try_clone()?;
+    let req_reader = Deserializer::from_reader(BufReader::new(&tcp)).into_iter::<Request>();
+    let mut writer = BufWriter::new(&tcp_cloned);
 
-        for req in req_reader {
-            let req = req?;
-            debug!(logger, "Receive request from {}: {:?}", peer_addr, req);
-
-            macro_rules! send_resp {
-                ($resp:expr) => {
-                    {
-                        let resp = $resp;
-                        serde_json::to_writer(&mut writer, &resp)?;
-                        writer.flush()?;
-                        debug!(logger, "Response sent to the {}: {:?}", peer_addr, resp);
-                    }
-                };
+    macro_rules! send_resp {
+        ($resp:expr) => {
+            {
+                let resp = $resp;
+                serde_json::to_writer(&mut writer, &resp)?;
+                writer.flush()?;
+                debug!(logger, "Response sent to the {}: {:?}", peer_addr, resp);
             }
-
-            match req {
-                Request::Get { key } => send_resp!(match self.engine.get(key) {
-                    Ok(value) => GetResponse::Ok(value),
-                    Err(e) => GetResponse::Err(format!("{}", e)),
-                }),
-                Request::Rm { key } => send_resp!(match self.engine.remove(key) {
-                    Ok(_) => RmResponse::Ok(()),
-                    Err(e) => RmResponse::Err(format!("{}", e))
-                }),
-                Request::Set { key, value } => send_resp!(match self.engine.set(key, value) {
-                    Ok(_) => SetResponse::Ok(()),
-                    Err(e) => SetResponse::Err(format!("{}", e))
-                }),
-            }
-        }
-
-        Ok(())
+        };
     }
+
+    for req in req_reader {
+        let req = req?;
+        debug!(logger, "Receive request from {}: {:?}", peer_addr, req);
+
+        match req {
+            Request::Get { key } => send_resp!(match engine.get(key) {
+                Ok(value) => GetResponse::Ok(value),
+                Err(e) => GetResponse::Err(format!("{}", e)),
+            }),
+            Request::Rm { key } => send_resp!(match engine.remove(key) {
+                Ok(_) => RmResponse::Ok(()),
+                Err(e) => RmResponse::Err(format!("{}", e))
+            }),
+            Request::Set { key, value } => send_resp!(match engine.set(key, value) {
+                Ok(_) => SetResponse::Ok(()),
+                Err(e) => SetResponse::Err(format!("{}", e))
+            }),
+        }
+    }
+
+    Ok(())
 }
 
 
